4ad4ba3baf0996b1453331549eaa1e5a58d4d916
subin <github@subin.kr>
2024-09-02 12:11:46 +0900

    UPDATE: Board component for scalability

--- src/components/board.svelte
+++ src/components/board.svelte
@@ -1,20 +1,16 @@
 <script>
-  import { Card, Button } from 'flowbite-svelte';
-  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { Card } from 'flowbite-svelte';
 
   export let img;
   export let title;
   export let summary;
-
-  let vCard = false;
+  export let horizontal;
 </script>
 
 <div class="space-y-4">
-  <Card {img} reverse={vCard}>
+  <Card {img} {horizontal}>
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{summary}</p>
-    <Button>
-      Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
-    </Button>
+    <slot />
   </Card>
 </div>
