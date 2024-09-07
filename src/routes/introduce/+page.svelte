cf8d498a70d9435a2b9c86b4bd8a23604c874ffa
subin <github@subin.kr>
2024-09-07 10:19:41 +0900

    MAKE: Slogan

--- src/routes/introduce/+page.svelte
+++ src/routes/introduce/+page.svelte
@@ -1,4 +1,6 @@
-<div class="max-w-8xl flex">
-  <img class="w-[50vw]" src="/images/people/ceo.jpg" alt="ceo" />
-  <div>CEO</div>
-</div>
+<script>
+  import Ceo from "./ceo.svelte";
+
+</script>
+
+<Ceo />
