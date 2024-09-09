18a2bda2cb133a45b43dff69c506b216c482a784
subin <github@subin.kr>
2024-09-09 11:37:55 +0900

    DROP: Not used code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,8 @@
 <script>
-  import Gallery from './gallery.svelte';
-  import MainScreen from './main-screen.svelte';
-  import { onMount } from 'svelte';
-  import { scrollY, bodyHeight, blueye6DivisionHeight, carouselHeight } from '$lib/scroll-control.ts';
-  import News from './news.svelte';
   import MainCarousel from './main-carousel.svelte';
   import Portfolio from './portfolio.svelte';
   import Recommend from './recommend.svelte';
-
-  let blueye6DivisionElement;
-  let carouselElement;
-
-  onMount(() => {
-    updateHeight(); // 초기 높이 설정
-  });
-
-  function updateHeight() {
-    // resize 이벤트 발생 시 높이 업데이트
-    $blueye6DivisionHeight = blueye6DivisionElement.clientHeight;
-    $carouselHeight = carouselElement.clientHeight;
-  }
 </script>
-
-<svelte:window on:resize={updateHeight} />
 
 <MainCarousel />
 <Portfolio />
