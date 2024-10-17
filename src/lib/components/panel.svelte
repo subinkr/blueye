bfe4575455e04b313c92e10d2efb968e4b58b40b
subin <github@subin.kr>
2024-10-17 17:25:18 +0900

    EDIT: 241017 base code

--- src/lib/components/panel.svelte
+++ src/lib/components/panel.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <a {href} class="hover:cursor-pointer relative w-60 h-full flex flex-col justify-between hover:shadow-xl hover:shadow-gray-500 rounded-lg">
-  <img class="object-cover h-32 rounded-t-lg" src={img} alt="" />
+  <img class="object-cover h-60 rounded-t-lg" src={img} alt="" />
   <slot />
 </a>
--- src/routes/(layout)/houses/[id]/main-images.svelte
+++ src/routes/(layout)/houses/[id]/main-images.svelte
@@ -11,19 +11,19 @@
     <img src={src} alt={src} />
   {/each}
 </Modal>
-<div class="w-full flex justify-center">
+<div class="w-full max-h-[400px] flex justify-center">
   <div class="max-w-[1200px] flex justify-center gap-4 px-4 lg:px-20">
-    <button class="flex-1"  on:click={() => (modal = true)}>
+    <button class="flex-1" on:click={() => (modal = true)}>
       {#if images.length > 0}
         <img class="w-full h-full object-cover" src={images[0]} alt={images[0]} />
       {/if}
     </button>
     <button class="hidden lg:flex w-[400px] flex-col gap-4" on:click={() => (modal = true)}>
       {#if images.length > 1}
-      <img src={images[1]} alt={images[1]} />
+      <img class="object-cover flex-1" src={images[1]} alt={images[1]} />
       {/if}
       {#if images.length > 2}
-      <img src={images[2]} alt={images[2]} />
+      <img class="object-cover flex-1" src={images[2]} alt={images[2]} />
       {/if}
     </button>
   </div>
