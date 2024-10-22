8753f851a0ed146ce358ba0a625722f6ae4d3794
subin <github@subin.kr>
2024-10-22 15:54:12 +0900

    EDIT: 241022 base code

--- src/routes/(layout)/houses/[id]/house-infos.svelte
+++ src/routes/(layout)/houses/[id]/house-infos.svelte
@@ -17,60 +17,60 @@
     <AlignCenter>
       <CashOutline size="xl" /><b>평균 분양가 (m²당 USD)</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.pricePerSquareMeter ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.pricePerSquareMeter.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <HomeOutline size="xl" /><b>판매 유형</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.salesType ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.salesType.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <RulerCombinedOutline size="xl" /><b>전용 면적 (m²)</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.squareMeter ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.squareMeter.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <CogOutline size="xl" /><b>구성</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.config ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.config.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <CalendarMonthOutline size="xl" /><b>입주 예정일</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.date ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.date.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <UsersGroupOutline size="xl" /><b>총 세대수</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.houseHolders ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.houseHolders.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <UserOutline size="xl" /><b>소유권 형태</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.own ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.own.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <BuildingOutline size="xl" /><b>개발 및 시공</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.builder ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.builder.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <DollarOutline size="xl" /><b>예상 임대 수익률</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.expectedReturn ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.expectedReturn.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
   <HouseInfo>
     <AlignCenter>
       <LandmarkOutline size="xl" /><b>취득세</b>
     </AlignCenter>
-    <div class="prose max-w-none">{@html marked(house?.tax ?? "")}</div>
+    <div class="prose max-w-none">{@html marked(house?.tax.split('\n').join('\n\n') ?? "")}</div>
   </HouseInfo>
 </div>
