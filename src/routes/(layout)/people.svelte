7801914128a5d937aea156f5b636690153f01153
subin <github@subin.kr>
2024-11-09 18:57:44 +0900

    EDIT: use people.id

--- src/routes/(layout)/people.svelte
+++ src/routes/(layout)/people.svelte
@@ -7,9 +7,9 @@
 <div class="flex flex-col gap-8">
   <Title>블루아이 사람들</Title>
   <Marquee speed={0.5} hoverSpeed={0}>
-    {#each peoples as people, peopleId}
+    {#each peoples as people}
     <div class="w-full flex flex-col justify-center items-center">
-      <img alt={people.name} src={"/images/people/" + peopleId + ".jpg"} />
+      <img alt={people.name} src={"/images/people/" + people.id + ".jpg"} />
       <div class="text-center">
         <div class="whitespace-nowrap">{people.team}</div>
         <div class="flex gap-1 justify-center items-center">
