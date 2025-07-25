008ad5316cfdb54900ba46c9a9c8574d8ab2e95f
subin <github@subin.kr>
2025-07-25 11:19:55 +0900

    EDIT: platforms links

--- src/lib/components/platforms.svelte
+++ src/lib/components/platforms.svelte
@@ -7,9 +7,9 @@
 </script>
 
 <div class="flex gap-4 px-3">
-  <a href="https://www.facebook.com/people/상해코리아부동산/100063680325644"><Facebook /></a>
-  <a href="https://www.youtube.com/@블루아이"><Youtube /></a>
-  <a href="https://www.instagram.com/shkorea_2002"><Instagram /></a>
+  <a href="https://www.facebook.com/blueye.asia.official"><Facebook /></a>
+  <a href="https://www.youtube.com/@blueyeasia"><Youtube /></a>
+  <a href="https://www.instagram.com/blueyeasia"><Instagram /></a>
   <a href="https://pf.kakao.com/_qpRxjxb"><Kakaotalk /></a>
   <a href="https://blog.naver.com/ggumproject"><Blog /></a>
 </div>
